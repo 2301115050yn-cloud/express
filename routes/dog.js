aa231d1db9b653734f317254bc7f4265dc5b6d63
cs <2301115050yn@cyber-u.ac.jp>
2025-11-28 19:29:03 +0900

    add new dog api

--- routes/dog.js
+++ routes/dog.js
@@ -1,19 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
-
 router.get('/', async (req, res) => {
-    console.log('DOG ROUTE HIT');  // ← ログ
-
-    request('https://dog.ceo/api/breeds/image/random', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            const data = JSON.parse(body);
-            res.json(data);
-        } else {
-            console.log('Dog API error:', error);
-            res.status(500).send('API error');
-        }
-    });
+request('https://dog.ceo/api/breeds/image/random', function (error, response,
+body) {
+if (!error && response.statusCode == 200) {
+const data = JSON.parse(body);
+res.json(data);
+}
 });
-
+})
 module.exports = router;
